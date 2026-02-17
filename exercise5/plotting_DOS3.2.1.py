3a5919d176c0de966f14db3fca7e8b80085738eb
oskar <oskar.harber@outlook.com>
2026-02-17 15:54:13 +0000

    bands analysis

--- exercise5/plotting_DOS3.2.1.py
+++ exercise5/plotting_DOS3.2.1.py
@@ -6,7 +6,7 @@ import re
 def read_data(i):
     
 
-    filename = f"beta_sn_dos_{i}.dat"   # change to beta, alpha, or quartz file
+    filename = f"quartz_dos_{i}.dat"   # change to beta, alpha, or quartz file
 
     # ---- Read Fermi energy from header ----
     with open(filename, "r") as f:
@@ -56,8 +56,77 @@ def plots():
     plt.legend()
     plt.show()
 
+
+def get_fermi_from_scf(scf_file):
+    with open(scf_file, "r") as f:
+        for line in f:
+            if "the Fermi energy is" in line:
+                return float(line.split()[4])
+
+
+def read_bands(filename, EF):
+    k_points = []
+    energies = []
+
+    with open(filename, "r") as f:
+        lines = f.readlines()
+
+    # First line contains nbnd and nks
+    header = lines[0]
+    nbnd = int(header.split("nbnd=")[1].split(",")[0])
+    
+    i = 1
+    while i < len(lines):
+        # Read k-point
+        k = list(map(float, lines[i].split()))
+        k_points.append(k)
+        i += 1
+
+        # Read nbnd energies
+        band_vals = []
+        while len(band_vals) < nbnd:
+            band_vals += list(map(float, lines[i].split()))
+            i += 1
+
+        energies.append(band_vals)
+
+    k_points = np.array(k_points)
+    energies = np.array(energies)
+
+    # ---- Construct cumulative k-path distance ----
+    k_dist = np.zeros(len(k_points))
+    for j in range(1, len(k_points)):
+        dk = np.linalg.norm(k_points[j] - k_points[j-1])
+        k_dist[j] = k_dist[j-1] + dk
+
+    # ---- Shift energies by Fermi energy ----
+    #energies -= EF
+
+    return k_dist, energies
+
+
+def plot_bands():
+    #EF = get_fermi_from_scf("quartz.scf.out")
+    EF = 2.5756
+    print("Fermi energy:", EF)
+
+    k_dist, energies = read_bands("quartz-bands.dat", EF)
+
+    plt.figure()
+
+    for band in range(energies.shape[1]):
+        plt.plot(k_dist, energies[:, band])
+
+    plt.axhline(EF, linestyle="--")
+    plt.xlabel("k-path distance")
+    plt.ylabel("Energy - EF (eV)")
+    plt.title("Quartz Band Structure")
+    plt.show()
+
+
 def main():
     plots()
+    plot_bands()
 
 
 if __name__ == "__main__":
