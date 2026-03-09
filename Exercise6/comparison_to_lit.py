67a51d8583cd841ad4e1d0860165c206046532b4
oskar <oskar.harber@outlook.com>
2026-03-09 17:06:42 +0000

    edits to the lit comparison it is now accurate

--- Exercise6/comparison_to_lit.py
+++ Exercise6/comparison_to_lit.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 c = 2.99792458e10 
 plt.figure()
 #################### espresso 
-data = np.loadtxt("si.ph-disp.gp")
+data = np.loadtxt("si.ph-disptrue.gp")
 
 nbands = data.shape[1] - 1
 for band in range(nbands):
@@ -13,24 +13,24 @@ for band in range(nbands):
 ########################## lit data
 for i in range(1,11,1):
 
-    filename = f"si.li{i}.gp"
+    filename = f"si_li{i}.gp"
     lit = np.loadtxt(filename)
-    if i <3:
+    if i <=3:
         x = lit[:, 0] *data[50,0]
         #turn frequency from THz to per cm 
         frequency = lit[:, 1] *1e12/ c
-        plt.scatter(x, frequency, marker="k",colour="r")
+        plt.scatter(x, frequency, marker="o",color="r",s=15)
     elif 8> i >3:
         x = data[100,0] -lit[:, 0]*(data[100,0]-data[50,0])
 
         #turn frequency from THz to per cm 
         frequency = lit[:, 1] *1e12/ c
-        plt.scatter(x, frequency, marker="k",colour="r")
+        plt.scatter(x, frequency, marker="o",color="r",s=15)
     else:
-        x = data[100,0] + lit[:,0]*(data[-1,0] - data[100,0])
+        x = data[100,0] + 2*lit[:,0]*(data[150,0] - data[100,0])
         #turn frequency from THz to per cm 
         frequency = lit[:, 1] *1e12/ c
-        plt.scatter(x, frequency, marker="o",colour="r")
+        plt.scatter(x, frequency, marker="o",color="r",s=15)
 #######################################plotting
 plt.axvline(x=data[0, 0], linewidth=0.5, color='k', alpha=0.5)
 plt.axvline(x=data[50, 0], linewidth=0.5, color='k', alpha=0.5)
